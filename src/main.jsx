3f405594dcb7b89b21a871b52f1e296f61212394
Brian <netzeus20@gmail.com>
2026-06-25 21:10:55 -0400

    Import global density bridge

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 import '@/lib/hasten-ui.css'
+import '@/lib/hasten-density-bridge.css'
 import '@/lib/hasten-ui.js'
 
 if ('serviceWorker' in navigator) {
